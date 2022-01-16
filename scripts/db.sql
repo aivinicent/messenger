50b669e83e9606b3f0986395520cae41bf0cb52f
unknown <aivinicent@gmail.com>
2022-01-16 16:08:25 +0300

    fix scripts

--- scripts/db.sql
+++ scripts/db.sql
@@ -7,7 +7,7 @@ ALTER SEQUENCE messenger.messages_id_seq OWNER TO postgres;
 CREATE TABLE IF NOT EXISTS messenger.messages (
     id bigint NOT NULL DEFAULT nextval('messenger.messages_id_seq' :: regclass),
     body text COLLATE pg_catalog."default" NOT NULL,
-    "timestamp" timestamp with time zone NOT NULL,
+    "timestamp" bigint NOT NULL,
     CONSTRAINT messages_pkey PRIMARY KEY (id)
 ) TABLESPACE pg_default;
 
